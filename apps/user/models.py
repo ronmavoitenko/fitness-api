c0156fd9b044ed90f34272f40616ef3d752603d1
ringo1206 <romavoitenko195@gmail.com>
2023-10-26 09:46:40 +0300

    f

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# from apps.tasks.models import Tasks
-# from apps.activity.models import Activity
 
 
 class User(AbstractUser):
@@ -10,7 +8,5 @@ class User(AbstractUser):
     profile_image = models.FileField(upload_to='media/profile_image', null=True)
     verification_code = models.CharField(max_length=5, null=True)
     is_verified = models.BooleanField(default=False)
-    # tasks = models.ManyToManyField(Tasks, related_name='tasks', blank=True)
-    # activity = models.ForeignKey(Activity, on_delete=models.CASCADE, null=True)
     modified_at = models.DateTimeField(auto_now=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
