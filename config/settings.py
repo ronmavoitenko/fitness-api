f6a49ee0768016a031e282583f1ed39847ec4fb9
ringo1206 <romavoitenko195@gmail.com>
2023-10-30 09:39:39 +0200

    code refactoring

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 100
